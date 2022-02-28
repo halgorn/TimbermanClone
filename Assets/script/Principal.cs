163754c503a387d87f253e0097f7be791d4836b5
Bruno Inácio <brunoinacio3000@hotmail.com>
2022-02-28 15:54:51 -0300

    Commit Inicial

---
+++ Assets/script/Principal.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Principal : MonoBehaviour
+{
+    
+    public GameObject jogador;
+    public GameObject JogadorParado;
+    public GameObject JogadorBate;
+
+    float escalaJogadorHorizontal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        escalaJogadorHorizontal = transform.localScale.x;
+
+        JogadorBate.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")){
+
+            if(Input.mousePosition.x > Screen.width/2){
+                bateDireita();
+            }else {
+                bateEsquerda();
+            }
+
+        }
+    }
+
+    void bateDireita(){
+        JogadorBate.SetActive(true);
+        JogadorParado.SetActive(false);
+        jogador.transform.position = new Vector2(1.1f, jogador.transform.position.y);
+        jogador.transform.localScale = new Vector2(escalaJogadorHorizontal,jogador.transform.localScale.y);
+        
+    }
+    void bateEsquerda(){
+        JogadorBate.SetActive(true);
+        JogadorParado.SetActive(false);
+        jogador.transform.position = new Vector2(-1.1f, jogador.transform.position.y);
+        jogador.transform.localScale = new Vector2(-escalaJogadorHorizontal,jogador.transform.localScale.y);
+    }
+
+    void VoltaAnimacao(){
+        JogadorBate.SetActive(false);
+        JogadorParado.SetActive(true);
+    }
+}
